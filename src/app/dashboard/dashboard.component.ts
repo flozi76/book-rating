19db7645deb68a3b715eac4d70a460ccffbe1dea
Florian Zimmermann <flozi76@bluewin.ch>
2022-11-30 13:45:01 +0100

    day 1 mittag

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'br-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent {
+  books: Book[] = []; // cannot be undefined. Initialized here or in constructor.
+  books2?: Book[]; // allow undefined;
+
+  constructor(){
+    this.books = [
+      {isbn: '1234', title: 'Angular', rating: 5, price: 33.9, description: 'Some dingda'},
+      {isbn: '7895', title: 'Rust', rating: 2, price: 77.6, description: 'Some rusty stuff'}
+    ];
+  }
+}
